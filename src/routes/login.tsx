7d11f1a69b6f767fec4273dde57a5fbd8ca003de
seila1828e3 <discorddelvictin@gmail.com>
2025-03-30 10:39:49 -0300

    sexo

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,49 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/login')({
+export const Route = createFileRoute("/login")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Login</div>
+  return (
+    <>
+      <header className="bg-light-gray">
+        <h1 className="text-center font-semibold text-2xl text-header-blue ">
+          LOGIN
+        </h1>
+
+        <div className="flex">
+          <div className="bg-header-red h-[10px] grow"></div>
+          <div className="bg-header-light-blue h-[10px] grow"></div>
+          <div className="bg-header-yellow h-[10px] grow"></div>
+        </div>
+      </header>
+
+      <main className="h-screen bg-dark-gray flex items-center justify-center">
+        <div className="columns-1">
+          <div className="mr-[70px]">
+            <h1 className="text-5xl font-inter font-light text-whitebor">
+              <em>CONECTA</em>
+            </h1>
+          </div>
+
+          <div className="ml-[100px]">
+            <h1 className="text-5xl font-inter font-light text-whitebor mb-[40px]">
+              <em>SEINFRA</em>
+            </h1>
+          </div>
+
+          <div className="rounded-3xl bg-light-gray">
+            <label className="">
+              <input
+                placeholder="CPF"
+                type="text"
+                className="px-8 py-3 text-header-blue font-inter font-semibold"
+              />
+            </label>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
