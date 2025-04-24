756180d6d3008873d68da8b5b098ab0b270eaae2
seila1828e3 <discorddelvictin@gmail.com>
2025-04-24 14:12:54 -0300

    ta indo

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,20 @@
 interface Props {
     title: string;
+    src: string;
 }
+
 
 export const Button_Input = (props: Props) => {
     return (
         <>
-        <div className="rounded-2xl bg-light-gray flex items-center space-x-16">
+        <div className="rounded-2xl bg-light-gray flex items-center justify-center space-x-3 " >
             <input type="text" 
             placeholder={props.title}
-            className="px-16 pr-16 py-4 text-header-blue font-inter font-bold cursor-pointer outline-0"
+            className="px-12 py-4 text-header-blue font-inter font-bold cursor-pointer outline-0 "
+            />
+            <img 
+            src={props.src}
+            className="mr-4"
             />
         </div>
         </>
--- src/routes/cadastro.tsx
+++ src/routes/cadastro.tsx
@@ -8,6 +8,8 @@ import telefone from "../assets/icons/telefone.png";
 import credencias from "../assets/icons/credenciais.png";
 import hide_gray from "../assets/icons/Hide_gray.png";
 import show_gray from "../assets/icons/Show_gray.png";
+import teclado from "../assets/icons/teclado.png";
+import { Footer } from "../components/footer";
 
 export const Route = createFileRoute("/cadastro")({
   component: RouteComponent,
@@ -20,27 +22,38 @@ function RouteComponent() {
       <div className="flex justify-center items-center bg-dark-gray text-light-gray h-screen">
         <div className="columns-1">
           <Title />
-          <form>
-          <div className="flex items-center justify-center">
-          <Button_Input title="Nome" />
-          <img src={pessoa} alt="pessoa_icon" className="w-[25px]" />
-          </div>
-          <br />
+          <main>
+            <form>
+              <Button_Input title="Nome" src={pessoa} />
 
-          <Button_Input title="Telefone"/>
-          <br />
+              <br />
 
-          <Button_Input title="CPF"/>
-          <br />
+              <Button_Input title="Telefone" src={telefone} />
+              <br />
 
-          <Button_Input title="Senha"/>
-          <br />
+              <Button_Input title="CPF" src={credencias} />
+              <br />
 
-          <Button_Input title="Confirmação da Senha"/>
+              <Button_Input title="Senha" src={hide_gray} />
+              <br />
 
-          </form>
+              <Button_Input title="Confirmação da Senha" src={teclado} />
+            </form>
+
+            <div className="flex items-center justify-center">
+              <button
+                type="submit"
+                className=" m-[20px] text-whitebor  bg-green-blue rounded-2xl disabled:bg-red-700 "
+              >
+                <h1 className="px-5.5 py-1.5 font-inter font-extrabold ">OK</h1>
+              </button>
+            </div>
+          </main>
         </div>
       </div>
+      <footer>
+        <Footer hasLogo={true} />
+      </footer>
     </>
   );
 }
